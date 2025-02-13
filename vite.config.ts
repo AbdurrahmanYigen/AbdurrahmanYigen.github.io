fecb9a391b39e63d3415be36b76faa1257c34d50
Abdurrahman Yigen <abdurrahman.yigen@iits-consulting.de>
2025-02-13 15:19:18 +0100

    change base path to "/"

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
